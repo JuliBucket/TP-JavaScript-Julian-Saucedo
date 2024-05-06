30623b6d0f3a6986b25bc1b4cfe6d8c5219cb045
JuliBucket <juliansaucedo92@gmail.com>
2024-05-06 09:37:23 -0300

    completo parImpar

---
+++ assets/js/parImpar.js
@@ -0,0 +1,11 @@
+let numero = parseFloat(prompt("ingrese un numero"))
+
+if(!isNaN(numero)){
+  if (numero%2 === 0){
+    console.log(numero + " es numero par")
+  } else{
+    console.log(numero + " es numero impar")
+  }
+} else{
+  console.log("ingrese un valor numerico")
+}
