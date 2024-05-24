91e78f4aa181a61ab403a7587bea7a4c08734a91
JuliBucket <juliansaucedo92@gmail.com>
2024-05-24 10:57:16 -0300

    Trabajo Practico Numero 3 finalizado

--- Trabajo_Practico_3/assets/js/tareas.js
+++ Trabajo_Practico_3/assets/js/tareas.js
@@ -1,46 +1,82 @@
 let task = []
+const regex = /^\S(.*\S)?$/;
 
 function agregar(element){
-  if(element){
-
+  if(regex.test(element)){
+    task.push(element)
+    alert(`Se agregó la tarea: `+ element)
+  } else {
+    alert(`No puede ingresar espacios vacios`)
   }
+  
 }
 
 function eliminar(element){
-    for (let i=0; i<task.length; i++){
+
+    if (task.includes(element)){
+      for (let i=0; i<task.length; i++){
         console.log(task[i])
         if(task[i]===element){
-            task.splice(i,1)  
+          task.splice(i, 1)
+
         }
     }
-}
-//prompt = Tuculo Tuculo == ' '
-function editar(posicion, element){{
-    task.splice(posicion, 1, element)
-  }
+      alert('Se eliminó la materia: '+ element)
+    }else{
+      alert('No coincide con ninguna materia')
+    }
 }
 
+function editarTareas(nombre){
+  if (task.includes(nombre)){
+      let nuevoNombre = prompt("Escribir nuevo nombre para la materia");
+      switch (true) {
+          case (nuevoNombre === ''):
+              alert('No se puede agregar una materia vacía.')
+              break;
+          case ((nuevoNombre.charAt(0) == ' ')):
+              alert('No se puede agregar una materia con espacios al principio.')
+              break;
+          case ((nuevoNombre.charAt(nuevoNombre.length - 1) == ' ')):
+              alert('No se puede agregar una materia con un espacio al final.')
+              break;
+          default:
+              alert('Se cambió la materia ' + nombre + " --> " + nuevoNombre);
+              task[task.indexOf(nombre)] = nuevoNombre;
+              break;
+  }
+      }
+      else{
+          alert('No existe esa materia')
+      }
+}
+
+
 function lista(){
-    console.log(task)
+    alert(task())
 }
 
 let opcion
 while (opcion != 0){
 opcion = parseInt(prompt('Ingrese una operacion\n1-Agregar una materia\n2-Eliminar materia\n3-Editar materia\n4-Lista de materias\n0-salir'))
   switch(opcion){
+    case 0:
+      alert(`Saliendo`)
+      break;
     case 1:
-        agregar (prompt('Ingrese el nombre de la tarea que desea añadir'))
+      agregar(prompt(`Ingrese una materia`))
       break;
     case 2:
-      eliminar(prompt(`ingrese la tarea a eliminar\n${task}`))
+      eliminar(prompt(`Ingrese la materia a eliminar\n${task}`))
       break;
     case 3:
-      editar(prompt(`ingrese la tarea que desea editar\n${task}`), prompt(`Ingrese el valor que quiere colocar`))
+      editarTareas(prompt(`Seleccione la materia que desea cambiar\n${task.join('\n')}`));
       break;
     case 4:
-      alert((`La lista tareas es:\n${task}`))
+      alert(`La lista de materias es:\n${task.join('\n')}`);
       break;
       default:
-        alert('Ingrese una opción válida.')
+        alert('Ingrese un valor válido')
+        break;
   }
 }
